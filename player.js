6378bb46da7bc12b584847d7605316e21dac7814
Robin Kranich <kontakt@robinkrani.ch>
2023-09-23 14:09:01 +0200

    added appdata function and notes

---
+++ player.js
@@ -0,0 +1,9 @@
+/**
+ * what to use for "playing an audio file"?
+ *
+ * https://www.npmjs.com/package/media-player-controller
+ * https://www.npmjs.com/search?q=keywords:ffplay
+ * https://www.npmjs.com/package/mpv
+ *
+ * media-player-controller controlling mpv sounds awesome!
+ */
---
+++ podcast-feed.js
@@ -0,0 +1,11 @@
+/**
+ *
+ *
+ * - cache
+ *   - feeds
+ *     - <hash-for-feed-url>
+ *       - feed.xml
+ *       - episodes
+ *         - <hash-for-episode>
+ *           - episode.mp3
+ */
